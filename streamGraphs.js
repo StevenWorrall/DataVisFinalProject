de8e73651b46ba9a71e4cb1ee992dbf5ccdbdee0
dymzd <daiya.masuda@gmail.com>
2018-05-07 00:45:37 -0700

    Update streamGraphs.js

--- streamGraphs.js
+++ streamGraphs.js
@@ -26,32 +26,31 @@ select.on("change", function() {
 	processed = selectValue.replace(/ /g, '');
 	
 	d3.csv('trendScore/'+processed+'.csv', function(err, d){
-	if(err) console.log(err);
-	//console.log(d)
-	var nested_data = d3.nest()
-		.key(function(d) { return d.Year; })
-		.entries(d);
+		if(err) console.log(err);
+		//console.log(d)
+		var nested_data = d3.nest()
+			.key(function(d) { return d.Year; })
+			.entries(d);
 
-	console.log(nested_data);
+			console.log(nested_data);
 
-	var trddata2 = nested_data.map(function(d){
-	var obj = {
-	    month: new Date(d.key)
-	 }
-	d.values.forEach(function(v){
-		obj[v.Keyword] = +v.Popularity;
+		var trddata2 = nested_data.map(function(d){
+		var obj = {
+		    month: new Date(d.key)
+		 }
+		d.values.forEach(function(v){
+			obj[v.Keyword] = +v.Popularity;
 
-		if(!keyarray.includes(d.Keyword)){
-		keyarray.push(d.Keyword);
+			if(!keyarray.includes(d.Keyword)){
+			keyarray.push(d.Keyword);
 
-	      }
-	 })
+		      	}
+		})
 		return obj;
-	})
+		})
 	buildStreamGraph2(trddata2);
+	})	
 })
-	
-}
 
 d3.csv('Data/demo.csv', function(err, d){
   if(err) console.log(err);
