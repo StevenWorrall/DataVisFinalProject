fe201ca6ecb7a65ab912baa692900345dfd51e80
dymzd <daiya.masuda@gmail.com>
2018-05-16 22:16:30 -0700

    Update streamGraphs.js

--- streamGraphs.js
+++ streamGraphs.js
@@ -408,12 +408,54 @@ svg.selectAll("path")
 	return color(i); })
     .on('mouseover', function(d){      
       d3.select(this).style('fill',d3.rgb( d3.select(this).style("fill") ).brighter());
-  		d3.select("#major").text(d.key);
+      d3.select("#major").text(d.key);
+      div.transition()	
+	.duration(200)
+	.style("opacity", .9);
+      var array = [];
+      array = d.values;
+      console.log(d);
+      keywordtemp = d.key;
+      var xPosition = d3.mouse(this)[0]-25;
+      var yPosition = d3.mouse(this)[1]-25;
+      yPosition = Math.floor(yPosition/4.65116279);
+      console.log(xPosition);
+      console.log(yPosition);
+      console.log(d[yPosition].data.month);
+      console.log(d[yPosition].data);
+      var temporary = {};
+      temporary = d[yPosition].data;
+      console.log(temporary);
+      console.log(keywordtemp);
+      console.log(temporary[keywordtemp]);
+      div.html(d.key + "<br/>" + formatYear(d[yPosition].data.month) + "<br/>"  + temporary[keywordtemp])	
+        .style("left", (d3.event.pageX) + "px")	
+        .style("top", (d3.event.pageY - 28) + "px");
     })
     .on('mouseout', function(d){      
       d3.select(this).style('fill', 
-         d3.rgb( d3.select(this).style("fill") ).darker());
-         d3.select("#major").text("KEYWORD (Hover over the graph) ");
+      d3.rgb( d3.select(this).style("fill") ).darker());
+      d3.select("#major").text("Mouse over");
+      div.transition()	
+         .duration(200)
+         .style("opacity", 0);
+})
+    .on('mousemove',function(d){
+      var xPosition = d3.mouse(this)[0]-25;
+      var yPosition = d3.mouse(this)[1]-25;
+      yPosition = Math.floor(yPosition/4.65116279);
+//       console.log(xPosition);
+      console.log(yPosition);
+      console.log(d[yPosition].data.month);
+      console.log(d[yPosition].data);
+      var temporary = {};
+      temporary = d[yPosition].data;
+      console.log(temporary);
+      console.log(keywordtemp);
+      console.log(temporary[keywordtemp]);
+      div.html(d.key + "<br/>" + formatYear(d[yPosition].data.month) + "<br/>"  + temporary[keywordtemp])	
+        .style("left", (d3.event.pageX) + "px")	
+        .style("top", (d3.event.pageY - 28) + "px");
 })
 
 svg.append("g")
